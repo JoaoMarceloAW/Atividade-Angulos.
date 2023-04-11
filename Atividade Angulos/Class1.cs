b19b64b7070fe078127ebdb3e8499f6875ae8bf9
Joao Marcelo <joaoamarceloalves585@gmail.com>
2023-04-11 11:03:10 -0300

    Add project files.

---
+++ Atividade Angulos/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Atividade_Angulos_
+{
+    internal class Angulo
+    {
+    
+    public    int ladoa;
+    public    int ladob;
+     public   int ladoc;
+
+        public string Calculo()
+        {
+
+            string resultado;
+
+          
+           if (ladoa == ladob || ladoa == ladoc || ladob == ladoc)
+            {
+
+                resultado = "Equilatero";
+            }
+         else  if (ladoa != ladob || ladoa != ladoc || ladob != ladoc)
+            { 
+                resultado = "Escaleno";
+               
+              
+
+            }
+           else
+            {
+                resultado = "Isósceles";
+            }
+            return resultado;
+           
+        }
+               
+
+    }
+}
---
+++ Atividade Angulos/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel.Design;
+using System.Net;
+using System.Xml;
+
+namespace Atividade_Angulos_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dados três valores X, Y, Z, verifiquem se eles podem ser os comprimentos dos lados de um triângulo e se forem escrever uma mensagem informando se é um triângulo eqüilátero, isósceles ou escaleno.
+
+            //O comprimento de um lado do triângulo é sempre menor do que a soma dos outros dois.•Eqüilátero: Todos os lados iguais
+
+            //•Isósceles: Dois lados iguais
+            //Escaleno: Todos os lados diferentes
+
+            
+
+
+       
+                Console.Clear();
+                   Angulo novoangulo = new Angulo();
+
+                Console.WriteLine("Bem vindo, programa para saber qual a classe do angulo!!!!");
+                Console.WriteLine("------------------------------------------------------------");
+                Console.WriteLine();
+
+
+                Console.WriteLine("Digite o lado A do seu angulo: ");
+                novoangulo. ladoa = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+
+
+                Console.WriteLine("Digite o lado B do seu angulo: ");
+                novoangulo. ladob = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+
+                Console.WriteLine("Digite o lado C do seu angulo: ");
+                novoangulo. ladoc = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(novoangulo.Calculo());
+
+                Console.Clear();
+                Console.WriteLine();
+
+             
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+        }
+    }
+}
